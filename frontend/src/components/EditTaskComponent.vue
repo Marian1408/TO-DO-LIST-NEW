3a9826f783255473174aaadb8c0eda3a707f2f58
emilio <emiliosalas54@gmail.com>
2025-08-10 01:22:44 -0600

    feat: Agrega componente para editar tareas y vista correspondiente

---
+++ frontend/src/components/EditTaskComponent.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="px-40 flex flex-1 justify-center py-5">
+    <div class="layout-content-container flex flex-col w-[512px] max-w-[512px] py-5 max-w-[960px] flex-1">
+      <div class="flex flex-wrap justify-between gap-3 p-4"><p class="text-[#0e1a13] tracking-light text-[32px] font-bold leading-tight min-w-72">Edit Task</p></div>
+      <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+        <label class="flex flex-col min-w-40 flex-1">
+          <p class="text-[#0e1a13] text-base font-medium leading-normal pb-2">Task Name</p>
+          <input
+            class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg text-[#0e1a13] focus:outline-0 focus:ring-0 border-none bg-[#e8f2ec] focus:border-none h-14 placeholder:text-[#51946b] p-4 text-base font-normal leading-normal"
+            value=""
+          />
+        </label>
+      </div>
+      <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+        <label class="flex flex-col min-w-40 flex-1">
+          <p class="text-[#0e1a13] text-base font-medium leading-normal pb-2">Project</p>
+          <select
+            class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg text-[#0e1a13] focus:outline-0 focus:ring-0 border-none bg-[#e8f2ec] focus:border-none h-14 bg-[image:--select-button-svg] placeholder:text-[#51946b] p-4 text-base font-normal leading-normal"
+          >
+            <option value="one"></option>
+            <option value="two">two</option>
+            <option value="three">three</option>
+          </select>
+        </label>
+      </div>
+      <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+        <label class="flex flex-col min-w-40 flex-1">
+          <p class="text-[#0e1a13] text-base font-medium leading-normal pb-2">Status</p>
+          <select
+            class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg text-[#0e1a13] focus:outline-0 focus:ring-0 border-none bg-[#e8f2ec] focus:border-none h-14 bg-[image:--select-button-svg] placeholder:text-[#51946b] p-4 text-base font-normal leading-normal"
+          >
+            <option value="one"></option>
+            <option value="two">two</option>
+            <option value="three">three</option>
+          </select>
+        </label>
+      </div>
+      <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+        <label class="flex flex-col min-w-40 flex-1">
+          <p class="text-[#0e1a13] text-base font-medium leading-normal pb-2">Priority</p>
+          <select
+            class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg text-[#0e1a13] focus:outline-0 focus:ring-0 border-none bg-[#e8f2ec] focus:border-none h-14 bg-[image:--select-button-svg] placeholder:text-[#51946b] p-4 text-base font-normal leading-normal"
+          >
+            <option value="one"></option>
+            <option value="two">two</option>
+            <option value="three">three</option>
+          </select>
+        </label>
+      </div>
+      <div class="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+        <label class="flex flex-col min-w-40 flex-1">
+          <p class="text-[#0e1a13] text-base font-medium leading-normal pb-2">Description</p>
+          <textarea
+            class="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg text-[#0e1a13] focus:outline-0 focus:ring-0 border-none bg-[#e8f2ec] focus:border-none min-h-36 placeholder:text-[#51946b] p-4 text-base font-normal leading-normal"
+          ></textarea>
+        </label>
+      </div>
+      <div class="flex px-4 py-3 justify-end">
+        <button
+          class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 bg-[#39e079] text-[#0e1a13] text-sm font-bold leading-normal tracking-[0.015em]"
+        >
+          <span class="truncate">Save</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AddTasksView from '@/views/AddTasksView.vue'
+import EditTasksView from '@/views/EditTasksView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,11 @@ const router = createRouter({
       path: '/add',
       name: 'add-task',
       component: AddTasksView,
+    },
+    {
+      path: '/edit',
+      name: 'edit-task',
+      component: EditTasksView,
     },
   ],
 })
