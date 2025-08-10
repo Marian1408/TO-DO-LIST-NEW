1b25034f6550dbda9b0488ea47c182df2d7c735d
emilio <emiliosalas54@gmail.com>
2025-08-10 01:16:01 -0600

    refactor: Simplifica la estructura de App.vue al reemplazar componentes por RouterView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-import HeaderComponent from '@/components/HeaderComponent.vue'
-import AddTaskComponent from '@/components/AddTaskComponent.vue'
+
 </script>
 
 <template>
-  <div class="relative flex size-full min-h-screen flex-col group/design-root overflow-x-hidden" style='font-family: Inter, "Noto Sans", sans-serif;'>
-    <div class="layout-container flex h-full grow flex-col">
-      <header-component/>
-      <add-task-component/>
-    </div>
-  </div>
+  <RouterView />
 </template>
 
 <style scoped>
