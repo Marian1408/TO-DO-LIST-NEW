e14f4c00e0001f2a8f6e0eb2e9c612b96e94dd24
emilio <emiliosalas54@gmail.com>
2025-08-11 00:50:24 -0600

    feat: Añadir componentes de botón para notificaciones, lista y añadir tareas

---
+++ frontend/src/components/ButtonBellComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-1 justify-end gap-8">
+    <div class="flex items-center gap-9">
+      <a class="text-[#0e1a13] text-sm font-medium leading-normal" href="#">Home</a>
+      <a class="text-[#0e1a13] text-sm font-medium leading-normal" href="#">Projects</a>
+      <a class="text-[#0e1a13] text-sm font-medium leading-normal" href="#">Team</a>
+    </div>
+    <button
+      class="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 bg-[#e8f2ec] text-[#0e1a13] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5"
+    >
+      <div class="text-[#0e1a13]" data-icon="Bell" data-size="20px" data-weight="regular">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
+          <path
+            d="M221.8,175.94C216.25,166.38,208,139.33,208,104a80,80,0,1,0-160,0c0,35.34-8.26,62.38-13.81,71.94A16,16,0,0,0,48,200H88.81a40,40,0,0,0,78.38,0H208a16,16,0,0,0,13.8-24.06ZM128,216a24,24,0,0,1-22.62-16h45.24A24,24,0,0,1,128,216ZM48,184c7.7-13.24,16-43.92,16-80a64,64,0,1,1,128,0c0,36.05,8.28,66.73,16,80Z"
+          ></path>
+        </svg>
+      </div>
+    </button>
+    <div
+      class="bg-center bg-no-repeat aspect-square bg-cover rounded-full size-10"
+      style='background-image: url("https://lh3.googleusercontent.com/aida-public/AB6AXuB-YpigIgzU8wlYMmWgkVfbx08QkQNj2X26rQqukuBdNp6unwBohS3WY1YSFnf8lnQgezLYm1n-BM4pODyuwaRhhFgnNYPvgkAnX-vNfRcjnDZKAN18AsOYniDA5rdqNnd-F94MEM30FKg5uqHyjkUhajJvf_n3NtExdRFD7G8LtcW6TrS9Gq9gbT2EBxgx-pcUm6SLRcIac-E2gZNCH0C0_RFIK-BQxLh6XuWmAvR6lCkpW7yBVku5zK_xznDaibp9fhuffWvXIVfn");'
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/ButtonPlusComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-1 justify-end gap-8">
+    <button
+      class="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 bg-[#e8f2ec] text-[#0e1a13] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5"
+    >
+      <div class="text-[#0e1a13]" data-icon="Plus" data-size="20px" data-weight="regular">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
+          <path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path>
+        </svg>
+      </div>
+    </button>
+    <div
+      class="bg-center bg-no-repeat aspect-square bg-cover rounded-full size-10"
+      style='background-image: url("https://lh3.googleusercontent.com/aida-public/AB6AXuBg6nxMkxrAipRpr-ZtzCDnGf510d8RVyFwM6hAw1XA1mdVikBzxRWFkCp9tGD6rOpomxeWeKs5yXTsOqnSjrMDVxWf2M06-ufs6_0vdFe45GBk0MqBsiimtQzU8RYAfxjwZALd6lHzaOIYY6-YvgVPrZjG9jU_XFTpY522QdqsPJeuIsIGdfau5k0X_OjIokoyz-42fkTjG0F-M_nCU-jJLktDZTo0_FEwl0uS-a3VfMkPgRs1CgZz1oDHrRb26vMeV-zWaldNV2ps");'
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
